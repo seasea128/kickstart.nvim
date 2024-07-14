2b1be8dcfa23eedffba5e150ea02485bf93e782b
seasea128 <seasea@windowslive.com>
2024-07-14 14:18:29 +0700

    use which-key 3.0 syntax + changing to modern theme

--- init.lua
+++ init.lua
@@ -302,6 +302,10 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
+      preset = 'modern',
+      layout = {
+         align = 'center',
+      },
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.opt.timeoutlen
       delay = 0,
