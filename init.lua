278efd582020fa38ef95968dedd05ec7b7efd257
seasea128 <seasea@windowslive.com>
2024-07-14 14:18:29 +0700

    use which-key 3.0 syntax + changing to modern theme

--- init.lua
+++ init.lua
@@ -301,16 +301,29 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      require('which-key').setup {
+        preset = 'modern',
+        layout = {
+          align = 'center',
+        },
+      }
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>b'] = { name = '[B]uffers', _ = 'which_key_ignore' },
+      --require('which-key').register {
+      --  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      --  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      --  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+      --  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      --  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      --  ['<leader>b'] = { name = '[B]uffers', _ = 'which_key_ignore' },
+      --}
+      require('which-key').add {
+        { '<leader>c', desc = '[C]ode' },
+        { '<leader>d', desc = '[D]ocument' },
+        { '<leader>r', desc = '[R]ename' },
+        { '<leader>s', desc = '[S]earch' },
+        { '<leader>w', desc = '[W]orkspace' },
+        { '<leader>b', desc = '[B]uffers' },
       }
     end,
   },
